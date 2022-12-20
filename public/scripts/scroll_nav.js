d315abe1a4a827849fa5625dee98af89abaaae18
Abos2024 <abos2024@gmail.com>
2022-12-20 18:28:29 +0300

    Homepage image

--- public/scripts/scroll_nav.js
+++ public/scripts/scroll_nav.js
@@ -5,8 +5,10 @@ window.onscroll = () => {
 scrollNavbar = () => {
   var navBar = document.getElementById("navbar");
   const links = document.querySelectorAll("#navbar .nav-active");
-  const text = document.querySelectorAll("#navbar .topnav-text");
-
+  const text = document.querySelectorAll(".slide1 .slide-bg");
+  const linkies = document.querySelectorAll("#navbar .links");
+  const title = document.querySelectorAll(".slide1 h1");
+  const title2 = document.querySelectorAll(".slide1 h2");
   if (document.documentElement.scrollTop > 50) {
     navBar.classList.add("fixed-header");
 
@@ -16,7 +18,19 @@ scrollNavbar = () => {
     }
     for (let i = 0; i < text.length; i++) {
       const element = text[i];
-      element.classList.add("topnav-text-active");
+      element.classList.add("bg-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = linkies[i];
+      element.classList.add("linkies-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = title[i];
+      element.classList.add("title-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = title2[i];
+      element.classList.add("title-active");
     }
   } else {
     navBar.classList.remove("fixed-header");
@@ -26,7 +40,19 @@ scrollNavbar = () => {
     }
     for (let i = 0; i < text.length; i++) {
       const element = text[i];
-      element.classList.remove("topnav-text-active");
+      element.classList.remove("bg-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = linkies[i];
+      element.classList.remove("linkies-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = title[i];
+      element.classList.remove("title-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = title2[i];
+      element.classList.remove("title-active");
     }
   }
 };
