0d6a5b142f028f96677c90a1de4db328568aaafa
Abos2024 <abos2024@gmail.com>
2022-12-23 23:02:23 +0300

    Minor

--- public/scripts/scroll_nav.js
+++ public/scripts/scroll_nav.js
@@ -9,6 +9,9 @@ scrollNavbar = () => {
   const linkies = document.querySelectorAll("#navbar .links");
   const title = document.querySelectorAll(".slide1 h1");
   const title2 = document.querySelectorAll(".slide1 h2");
+  const logo = document.querySelectorAll("nav div:nth-child(1) a img");
+  const logotext = document.querySelectorAll("nav div:nth-child(1) span");
+  const navdivs = document.querySelectorAll("nav div");
   if (document.documentElement.scrollTop > 50) {
     navBar.classList.add("fixed-header");
 
@@ -32,6 +35,18 @@ scrollNavbar = () => {
       const element = title2[i];
       element.classList.add("title-active");
     }
+    for (let i = 0; i < text.length; i++) {
+      const element = logo[i];
+      element.classList.add("logo-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = logotext[i];
+      element.classList.add("logo2-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = navdivs[i];
+      element.classList.add("navd-active");
+    }
   } else {
     navBar.classList.remove("fixed-header");
     for (let i = 0; i < links.length; i++) {
@@ -53,6 +68,18 @@ scrollNavbar = () => {
     for (let i = 0; i < text.length; i++) {
       const element = title2[i];
       element.classList.remove("title-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = logo[i];
+      element.classList.remove("logo-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = logotext[i];
+      element.classList.remove("logo2-active");
+    }
+    for (let i = 0; i < text.length; i++) {
+      const element = navdivs[i];
+      element.classList.remove("navd-active");
     }
   }
 };
