a736db7ef4adda3381e9a52ab571e34e3808889c
JIS Grade 12 <111739930+jisgrade12@users.noreply.github.com>
2022-12-27 11:50:40 +0300

    Minor

--- public/scripts/swiper.js
+++ public/scripts/swiper.js
@@ -1,16 +1,10 @@
 const swiper = new Swiper(".swiper", {
   slidesPerView: 3,
+  spaceBetween: 30,
   direction: "horizontal",
   loop: true,
-
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    clickable: true,
   },
 });
