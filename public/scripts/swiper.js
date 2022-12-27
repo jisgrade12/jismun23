6763bc3efeb8a6a0442f4ce459755ee722ea2945
Abos2024 <abos2024@gmail.com>
2022-12-27 10:51:53 +0300

    Minor

---
+++ public/scripts/swiper.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
