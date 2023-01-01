98ce61390963021a7253bf46a2b50c942c285332
Abos2024 <abos2024@gmail.com>
2023-01-01 18:54:52 +0300

    Committee Page (and Minor)

--- public/scripts/swiper.js
+++ public/scripts/swiper.js
@@ -7,4 +7,8 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: ".swiper-button-next-unique",
+    prevEl: ".swiper-button-prev-unique",
+  },
 });
