05efee9c11c492b4aa0dfe3cfa42374d75ec9a89
JIS Grade 12 <111739930+jisgrade12@users.noreply.github.com>
2023-01-08 11:40:05 +0300

    Minor

--- public/scripts/countdown.js
+++ public/scripts/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 17, 2023 07:45:00").getTime();
+var countDownDate = new Date("Feb 10, 2023 07:45:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
